1d6cad9c2b3195ebfd96a7bda6fb0f80f2cd1e10
Ethan Cox <ethanecx@gmail.com>
2013-12-14 17:00:00 +0000

    added start of angle control

---
+++ Qwiddtch/components/angle.py
@@ -0,0 +1,17 @@
+try:
+    import wpilib
+except ImportError:
+    import fake_wpilib as wpilib
+    
+class angle(object):
+  
+  def __init__(self, angle_servo):
+    self.angle_servo = angle_servo
+    
+  def set_angle(d_anlge):
+    self.d_angle = d_angle
+    
+  def update():
+    
+    
+    
