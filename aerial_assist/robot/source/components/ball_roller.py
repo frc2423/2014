8fa5e3d3eec009fa57326edec715f322b2398a49
Ghislaine Coulter-de Wit <Ghi@Ghislaines-MacBook-Air.local>
2014-01-23 19:50:45 -0500

    Trying to fix stuff.

--- aerial_assist/robot/source/components/ball_roller.py
+++ aerial_assist/robot/source/components/ball_roller.py
@@ -3,22 +3,40 @@ try:
 except ImportError:
     from pyfrc import wpilib
 
+    #States
+    automatic_spd = 1
+    manual_spd = 2
+
+    #Constants
+    '''Place holders for now'''
+    BALL_NEAR = 1
+
+
 class BallRoller():
+
+    '''Controls the wheels that roll the ball in/out, hence the name'''
 
     def __init__(self, ball_roller_motor, ball_roller_sensor):
 
         self.ball_roller_motor = ball_roller_motor
         self.ball_roller_sensor = ball_roller_sensor
+        self.mode = None
 
     def automatic_spd(self, ball_near, br_forward):
 
-        if ball_roller_sensor.Get() <= ball_near:
-            self.ball_roller_motor.Set(br_forward)
-        else self.ball_roller_motor.Set(0)
+        self.mode = automatic_spd
 
     def manual_spd(self, br_manual_spd):
-        ball_roller.motor.Set(br_maunual_spd)
+        
+        self.mode = manual_spd
 
     def update(self):
 
-        #needs to check if the ball is there
+        if self.mode == automatic_spd and self.ball_roller_sensor.Get() <= BALL_NEAR:
+            self.ball_roller_motor.Set(br_forward)
+
+        if self.mode == automatic_spd and self.ball_roller_sensor.Get() >= BALL_NEAR:
+            self.ball_roller_motor.Set(0)
+
+        if self.mode == manual_spd:
+            self.ball_roller.Set(maunual_spd)
