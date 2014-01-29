8f3364e5719a6deb48252af76c1f436623bc2e83
unknown <ethanecx@gmail.com>
2014-01-28 19:28:06 -0500

    did some naming stuff in igus_slide

--- aerial_assist/robot/source/components/igus_slide.py
+++ aerial_assist/robot/source/components/igus_slide.py
@@ -4,11 +4,11 @@ except ImportError:
 	from pyfrc import wpilib
 
 #States	
-shoot = 0 #next action is to shoot
+SHOOT = 0 #next action is to shoot
 pull_back = 1 #next action
-shooting = 3 #in the process of shooting
-manual_winch = 4 #manual control of the winch
-slow_release = 5 #about to release the slide slowy not sure if possible
+SHOOTING = 3 #in the process of shooting
+MANUAL_WINCH = 4 #manual control of the winch
+SLOW_RELEASE = 5 #about to release the slide slowy not sure if possible
 
 #Constants not definite values yet
 IGUS_SENSOR_DISTANCE = 1
@@ -24,7 +24,7 @@ class igus_slide(object):
 			igus limit switch 		make sure the slide is all the way back
 			igus solenoid			to use the winch quick release
 			igus distance sensor	for detecting if ball is in the correct position
-			igus optical sensor	for detecting if slide is in the correct position for loading mode
+			igus optical limit	for detecting if slide is in the correct position for loading mode
 			
 	'''
 	def __init__(self, igus_motor, igus_limit_switch, igus_solenoid, igus_distance, igus_opt_limit_switch):
@@ -45,23 +45,19 @@ class igus_slide(object):
 	def manual_winch(self, pull_speed):
 		'manually controls the winch'
 		self.pull_speed = pull_speed
-		self.mode = manual_winch
+		self.mode = MANUAL_WINCH
 		
 	def slow_release(self):
-		self.mode = slow_release
+		self.mode = SLOW_RELEASE
 		
 	def shoot(self):
-		self.mode = shoot
+		self.mode = SHOOT
 	
 	def pull_back(self):
 		self.mode = pull_back
 		
 	def ready_to_load(self):
-		if self.igus_opt_limit_switch.Get() <= RAIL_LOADING_DISTANCE:
-			return True
-		
-		else:
-			return False
+		return self.igus_opt_limit_switch.Get()
 	
 	def update(self):
 		if self.mode == pull_back:
@@ -73,21 +69,19 @@ class igus_slide(object):
 			if self.igus_limit_switch == True:
 				self.igus_solenoid.Set(False)
 				self.mode = None
-				self.ready = True 
 			
-		elif self.mode == shoot:
+		elif self.mode == SHOOT:
 			
 			self.igus_solenoid.Set(True)
 			self.mode = releasing
 
-		elif self.mode == shooting:
+		elif self.mode == SHOOTING:
 			'make sure ball launches fully before stopping again' 
 			wpilib.wait(BALL_RELEASE_WAIT_TIME)
 			self.igus_solenoid.Set(False)
 			self.mode = None
-			
-			
-		elif self.mode == slow_release: 
+		
+		elif self.mode == SLOW_RELEASE: 
 			if self.igus_slide_opt_limit_switch >= RELEASE_DISTANCE:
 				'slowly move the slide forward instead of switching the gear, not sure if possible'
 				self.igus_motor.Set(RELEASE_SPEED)
@@ -96,7 +90,7 @@ class igus_slide(object):
 				self.igus_motor.Set(0)
 				self.mode = None
 		
-		elif self.mode == manual_winch:
+		elif self.mode == MANUAL_WINCH:
 			'manual control of the winch'
 			self.igus_motor.Set(self.pull_speed)
 			self.mode = None
