e622083b590464e0997521a14b0e5dd7d81defd5
srosenblum <rosenblum.sam@gmail.com>
2014-02-15 10:27:26 -0500

    test check in

--- aerial_assist/debug/electrical test/tests/basic_test.py
+++ aerial_assist/debug/electrical test/tests/basic_test.py
@@ -1,15 +1,30 @@
+#
+# This is a py.test based testing system. Anything you can do with
+# py.test, you can do with this too. There are a few magic parameters
+# provided as fixtures that will allow your tests to access the robot
+# code and stuff
+#
 #    robot - This is whatever is returned from the run function in robot.py
 #    wpilib - This is the wpilib module
+#
+
+
+#
+# Each of these functions is an individual test run by py.test. Global
+# wpilib state is cleaned up between each test run, and a new robot
+# object is created each time
+#
 
 def test_autonomous(robot, wpilib):
     
     wpilib.internal.enabled = True
     robot.Autonomous()
+    print('auto')
 
 
 def test_disabled(robot):
     robot.Disabled()
-
+    print('dis')
 
 def test_operator_control(robot, wpilib):
     
@@ -18,18 +33,29 @@ def test_operator_control(robot, wpilib):
     
         loop_count = 0
         
-        def IsOperatorControl(self):
+        stick_prev = 0
+        
+        def IsOperatorControl(self, tm):
             '''
                 Continue operator control for 1000 control loops
+                
+                The idea is to change the joystick/other inputs, and see if the 
+                robot motors/etc respond the way that we expect. 
+                
+                Keep in mind that when you set a value, the robot code does not
+                see the value until after this function returns. So, when you
+                use assert to check a motor value, you have to check to see that
+                it matches the previous value that you set on the inputs, not the
+                current value.
             '''
             self.loop_count += 1
             
-            
             return not self.loop_count == 1000
-
+    
+    print('argh')
+    wpilib.internal.set_test_controller(TestController)
     wpilib.internal.enabled = True
     
     robot.OperatorControl()
     
     # do something like assert the motor == stick value
-
